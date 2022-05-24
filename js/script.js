a6c249475a8d200826912f4b661be36c2a2e3338
Eshgin <eshgin01@mail.ru>
2022-05-24 16:03:57 +0300

    Add clear button

--- js/script.js
+++ js/script.js
@@ -3,14 +3,23 @@
 const fingerSize = document.querySelector('.size'),
       thickness = document.querySelector('.thickness'),
       btn  = document.querySelector('.button'),
+      btnClear = document.querySelector('.button__clear'),
       long = document.querySelector('.long');
 
-console.log(fingerSize)
-
 btn.addEventListener('click', () => {
-    sumTotal(fingerSize, thickness, long)
+    sumTotal(fingerSize, thickness, long);
 })
 
 function sumTotal (size, thickness, long){
     long.value = (+size.value + +thickness.value) * 3.14;
+}
+
+btnClear.addEventListener('click', () => {
+    clearValues(fingerSize, thickness, long);
+})
+
+function clearValues (size, thickness, long){
+    size.value = null;
+    thickness.value = null;
+    long.value = null;
 }
